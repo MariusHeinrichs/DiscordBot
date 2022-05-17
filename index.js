b577cd1e2b99bb5395154810e0b2dd8fbcf82a99
Marius Heinrichs <marius.heinrichs@alumni.fh-aachen.de>
2022-05-17 16:49:07 +0200

    Antworten und Begrüßung

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const Discord = require('discord.js');
+const hereinspaziertChannelId = "976130777878249522"
 
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const Discord = require('discord.js')
+
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"] })
 
 
 client.on('ready', () => {
     console.log('Schmartbot bereit zum denken');
 });
 
-client.login('OTc2MTEwNzQyMTc0NTg4OTc4.GPCp-C.K5dbFGSFmGISiydyDvcKP4cfJ0S-s7Y9VNW2PI');
+client.on('messageCreate', (message) => {
+    if (message.content == "hi"){
+        message.reply("Hallo")
+    }
+})
+
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.get(hereinspaziertChannelId).send(`<@${member.id}> Herein Spaziert!`)
+})
+
+client.login('OTc2MTEwNzQyMTc0NTg4OTc4.GPCp-C.K5dbFGSFmGISiydyDvcKP4cfJ0S-s7Y9VNW2PI')
