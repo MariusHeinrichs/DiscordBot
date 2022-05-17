e49bf809d3600faf7667fde29844146c2888ef00
Marius Heinrichs <marius.heinrichs@alumni.fh-aachen.de>
2022-05-17 16:19:37 +0200

    Bot ist Online

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
 
-client.once('ready', () => {
+client.on('ready', () => {
     console.log('Schmartbot bereit zum denken');
 });
 
-
-
-
-
-//Keep at end of File
-client.login('T8RQON8sCvkW8RkkwK-aDh4aShlKNV7a');
+client.login('OTc2MTEwNzQyMTc0NTg4OTc4.GPCp-C.K5dbFGSFmGISiydyDvcKP4cfJ0S-s7Y9VNW2PI');
