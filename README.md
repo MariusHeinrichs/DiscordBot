e70b8f2bc9b70fa75f01ec1dce6e82df491834a8
Marius Heinrichs <marius.heinrichs@alumni.fh-aachen.de>
2022-05-17 15:36:38 +0200

    Geh Online!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DiscordBot
 Ein einfacher Discord Bot
+
+Bot Client Secret : T8RQON8sCvkW8RkkwK-aDh4aShlKNV7a
---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+    console.log('Schmartbot bereit zum denken');
+});
+
+
+
+
+
+//Keep at end of File
+client.login('T8RQON8sCvkW8RkkwK-aDh4aShlKNV7a');
